333f70a53529c812dbd5145242257ad30daa86c6
Chase Allen <ccallen001@gmail.com>
2022-11-01 10:02:44 -0400

    move error handling to middleware

--- server/index.js
+++ server/index.js
@@ -6,30 +6,45 @@ const cors = require('cors');
 
 const Note = require('./models/note');
 
+const unknownInput = (req, res, next) =>
+  res.status(404).send({ error: 'unknown endpoint' });
+
+const errorHandler = (error, _, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'mal formatted id' });
+  }
+
+  next(error);
+};
+
 app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
+app.use(unknownInput);
+app.use(errorHandler);
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2022-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2022-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2022-05-30T19:20:14.298Z',
-    important: true
-  }
-];
+// let notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     date: '2022-05-30T17:30:31.098Z',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only Javascript',
+//     date: '2022-05-30T18:39:34.091Z',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     date: '2022-05-30T19:20:14.298Z',
+//     important: true
+//   }
+// ];
 
 app.get('/', (_, res) => {
   res.send('<h1>Hello World!</h1>');
@@ -45,13 +60,10 @@ app.get('/api/notes', (_, res) => {
     });
 });
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
     .then((note) => (note ? res.json(note) : res.status(404).end()))
-    .catch((err) => {
-      console.error(err);
-      response.status(400).send({ error: 'mal formatted id' });
-    });
+    .catch((err) => next(err));
 });
 
 app.post('/api/notes', (req, res) => {
