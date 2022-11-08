7ed0dc56651216e26a50d88a9c2dce3b650bd7fe
Chase Allen <ccallen001@gmail.com>
2022-11-08 09:43:19 -0500

    add mongoose validation

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ const errorHandler = (error, _, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'mal formatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message });
   }
 
   next(error);
@@ -24,27 +26,6 @@ const errorHandler = (error, _, res, next) => {
 app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
-
-// let notes = [
-//   {
-//     id: 1,
-//     content: 'HTML is easy',
-//     date: '2022-05-30T17:30:31.098Z',
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: 'Browser can execute only Javascript',
-//     date: '2022-05-30T18:39:34.091Z',
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: 'GET and POST are the most important methods of HTTP protocol',
-//     date: '2022-05-30T19:20:14.298Z',
-//     important: true
-//   }
-// ];
 
 app.get('/', (_, res) => {
   res.send('<h1>Hello World!</h1>');
@@ -66,7 +47,7 @@ app.get('/api/notes/:id', (req, res, next) => {
     .catch((err) => next(err));
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body;
 
   if (!body.content) {
@@ -80,15 +61,17 @@ app.post('/api/notes', (req, res) => {
   })
     .save()
     .then((savedNote) => res.json(savedNote))
-    .catch((err) => console.error(err));
+    .catch((err) => next(err));
 });
 
 app.put('/api/notes/:id', (req, res, next) => {
-  const body = req.body;
+  const { content, important } = req.body;
 
-  const note = { content: body.content, important: body.important };
-
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    req.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then((updatedNote) => res.json(updatedNote))
     .catch((err) => next(err));
 });
